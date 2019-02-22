983a0b92253d55de58180f506f7c38ce64316906
Al Boss <alboss@sisypheans.org>
2019-02-22 13:09:40 -0800

    Customizing main nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,35 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import styles from './navigation.module.css'
+import React from 'react';
+import Link from 'gatsby-link';
+import styles from './navigation.module.css';
 
 export default () => (
-  <nav role="navigation">
+  <nav role='navigation'>
+    <a href='#' class='brand-logo'><img
+                                     src='images/body/Logo.svg'
+                                     class='responsive-img nav-logo'
+                                     width='268'
+                                     height='48' /></a>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
+        <Link to='/household/'> Household
+        </Link>
       </li>
       <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
+        <Link to='/businesses/'> Businesses
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to='/news-events/'> News & Events
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to='/language-resources/'> Language Resources
+        </Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to='/customer-service/'> Customer Service
+        </Link>
       </li>
     </ul>
   </nav>
-)
+);
